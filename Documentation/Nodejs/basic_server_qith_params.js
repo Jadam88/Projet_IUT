928d6644e85c245aaddbb81d1547ba5631b51f65
ADAM Jason <31762640+Jadam88@users.noreply.github.com>
2017-10-06 11:53:13 +0200

    Create basic_server_qith_params.js

---
+++ Documentation/Nodejs/basic_server_qith_params.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+
+var server = http.createServer(function(req, res) {
+    var params = querystring.parse(url.parse(req.url).query);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    if ('prenom' in params && 'nom' in params) {
+        res.write('Vous vous appelez ' + params['prenom'] + ' ' + params['nom']);
+    }
+    else {
+        res.write('Vous devez bien avoir un prénom et un nom, non ?');
+    }
+    res.end();
+});
+server.listen(8080);
