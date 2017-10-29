c503ff1ec7359400f1bd00a67fc732b5d3512401
ADAM Jason <31762640+Jadam88@users.noreply.github.com>
2017-10-29 16:15:29 +0100

    Create PIR_Console.ino

---
+++ PIR/Arduino/PIR_Console.ino
@@ -0,0 +1,38 @@
+#include <Bridge.h>//On démarre la librarie Bridge pour communiquer entre Linino et atmega32u4
+#include <Console.h>
+#include <Process.h>
+
+int PIR = 2;               // Entree sur laquelle est connecte le capteur
+int Etat_PIR = LOW;             // Au debut le capteur ne detecte pas de mouvement 
+int Val_PIR = 0;                    // on stocke la valeur du capteur
+
+Process p;//creer l'objet p de la classe Process 
+
+void setup() {
+  
+  //------------------E/S----------------------//
+  pinMode(LED_BUILTIN, OUTPUT);//On definit la led L13 comme une sortie
+  pinMode(PIR, INPUT); //On definit le pir comme une entree
+  //-------------------------------------------//
+  Bridge.begin();
+  Console.begin();
+  Console.println("Debut");
+}
+
+void loop() {
+  Val_PIR = digitalRead(PIR);//Lecture de la valeur deu PIR 
+  
+   if (Val_PIR == 1 )//Si detection de mouvement 
+    {
+     Console.println("INTRUS DETECTE");//On affiche un message
+     digitalWrite(LED_BUILTIN, HIGH);//On allume la led L13
+      Etat_PIR = HIGH ;//On definit la valeur de la variable ETAT_PIR a 1
+   }
+   else //Sinon
+   {
+    Console.println("Pas de mouvement");
+    digitalWrite(LED_BUILTIN, LOW);//La led reste eteinte
+    Etat_PIR = LOW ;//La valeur de la variable Etat_PIR reste a 0
+   }
+  delay(2500);//temporisation de 2,5 secondes
+}
